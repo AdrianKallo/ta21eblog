6f0e52f2da1ed9f2d81c12826adf295ca4638beb
AdrianKallo <97944193+AdrianKallo@users.noreply.github.com>
2024-02-14 13:27:12 +0200

    daisyui

--- resources/views/partials/simple-pagination.blade.php
+++ resources/views/partials/simple-pagination.blade.php
@@ -1,19 +1,23 @@
-@if ($paginator->hasPages())
-    <nav>
-        <ul class="pagination">
+<div class="join grid grid-cols-2">
+    @if ($paginator->hasPages())
+        <nav role="navigation" aria-label="Pagination" class="pagination">
             {{-- Previous Page Link --}}
-            @if ($paginator->onFirstPage())
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.previous')</span></li>
-            @else
-                <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
-            @endif
+            <button class="join-item btn btn-outline">
+                @if ($paginator->onFirstPage())
+                    <span class="pagination__link pagination__link--disabled">Previous</span>
+                @else
+                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="pagination__link">Previous</a>
+                @endif
+            </button>
 
             {{-- Next Page Link --}}
-            @if ($paginator->hasMorePages())
-                <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
-            @else
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.next')</span></li>
-            @endif
-        </ul>
-    </nav>
-@endif
+            <button class="join-item btn btn-outline">
+                @if ($paginator->hasMorePages())
+                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="pagination__link">Next</a>
+                @else
+                    <span class="pagination__link pagination__link--disabled">Next</span>
+                @endif
+            </button>
+        </nav>
+    @endif
+</div>
